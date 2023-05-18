21dcffce76226e95eba07f5eb021b83025e76a47
Deepankar <dip2leo@gmail.com>
2023-05-18 17:28:01 -0400

    added react files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        Looking good! 
+      </header>
+    </div>
+  );
+}
+
+export default App;
