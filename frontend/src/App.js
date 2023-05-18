a88d2d826f5a2a7409a97b00137611a3e00a6f17
Deepankar <dip2leo@gmail.com>
2023-05-18 18:22:54 -0400

    added top movie and details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Top10MoviesPage from './pages/top10movie/Top10Movie'
+import MovieDetailsPage from './pages/MovieDetails/MovieDetails';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="">
-        Looking good! 
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="/top10movies" element={<Top10MoviesPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to our home page!</h1>
+            <p>This is a simple React home page.</p>
+            <Link to='/top10movies'> Go to top 10 movies!</Link>
+        </div>
+    );
+}
+
+export default Home;
