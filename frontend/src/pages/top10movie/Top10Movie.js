c22dbdcc735dbcb48d566575484947d4b4ca1bf3
Deepankar <dip2leo@gmail.com>
2023-05-22 03:18:18 -0400

    fix: routes with id

--- frontend/src/pages/top10movie/Top10Movie.js
+++ frontend/src/pages/top10movie/Top10Movie.js
@@ -24,16 +24,16 @@ function Top10MoviesPage() {
             <div class="flex flex-row flex-wrap	box-border m-5 justify-center">
             {movies.map(movie => (
                     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4" key={movie.index} >
-                        <a href={`/movie/${movie.index}`}>
+                        <a href={`/movie/${movie.id}`}>
                             <img class="rounded-t-lg" src="https://todaysparent.mblycdn.com/uploads/tp/2018/04/best-kids-movies-us-netflix-sing.jpg" alt="" />
                         </a>
                         <div class="p-5">
-                            <a href= {`/movie/${movie.index}`}>
+                            <a href= {`/movie/${movie.id}`}>
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.Movie}</h5>
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Ranking: {movie.index + 1} </p>
-                            <a href= {`/movie/${movie.index}`}class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <Link to={`/movie/${movie.index}`}>View Details</Link>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Ranking: {movie.rank} </p>
+                            <a href= {`/movie/${movie.id}`}class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                <Link to={`/movie/${movie.id}`}>View Details</Link>
                                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
                         </div>
