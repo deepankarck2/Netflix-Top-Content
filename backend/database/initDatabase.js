e7998b84e844c98fd8e8711006c9c6b466ebf32f
Deepankar Chakraborty <52084764+deepankarck2@users.noreply.github.com>
2023-05-25 11:33:08 -0400

    Update initDatabase.js

--- backend/database/initDatabase.js
+++ backend/database/initDatabase.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const DB = new Sequelize(
     process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME,  process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
-    dialect: process.env.MYSQL_DIALECT
+    dialect:'mysql'
 });
 
 module.exports = DB;
